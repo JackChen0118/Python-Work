f6b362529df8524700d3b81a2138da0cfad595f2
JackChen0118 <108701045@nccu.edu.tw>
2019-12-24 22:04:49 +0800

    12/24上傳

---
+++ 數軟上課程式/egg/spam.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+a=np.random.randint(1,101)
+print(f'你今天的信運數字是 {a}!')
